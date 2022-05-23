8a8f48b2c2118f089c54d81b6b6c87884ecc7a89
ayamoosa123 <ayamoosa98@gmail.com>
2022-05-22 17:20:36 -0700

    make base game

---
+++ script.js
@@ -0,0 +1,41 @@
+const container = document.querySelector("#container");
+const grid = document.createElement('div')
+
+// soon will take input
+function takeInput(){
+    let userNum = prompt("Pick a number between 1 and 100.");
+    while(userNum < 0 || userNum > 100){
+      userNum = prompt("Pick a number between 1 and 100.")
+    }
+        return userNum;
+}
+
+let num = takeInput();
+
+// container.setAttribute('style', `grid-template-columns: repeat(${num}, 2fr); grid-template-rows: repeat(${num}, 2fr);`);
+
+
+//make grid
+for(let i=0; i < (num*num); i++){
+    const grid = document.createElement('div')
+    grid.classList.add("grid");
+    container.appendChild(grid);
+    let percent = 600/num;
+    grid.style.width = percent-1 + "px";
+    grid.style.height = percent-1 + "px";
+}
+
+const countAll = document.querySelectorAll('.grid').length;
+console.log(countAll);
+
+
+//make hover action
+let box = container.children
+for(let i=0; i < (num*num); i++){
+    box[i].addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "black";
+    });
+}
+
+
+
