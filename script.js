9be5e0a3b2c87936f8bc1f85acb9492d2cc09c4c
ayamoosa123 <ayamoosa98@gmail.com>
2022-05-23 17:47:47 -0700

    attempt buttons

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 const container = document.querySelector("#container");
 const grid = document.createElement('div')
+const gridChoice = document.querySelector(".grid-choice")
 
-// soon will take input
+
+
+
 function takeInput(){
     let userNum = prompt("Pick a number between 1 and 100.");
     while(userNum < 0 || userNum > 100){
       userNum = prompt("Pick a number between 1 and 100.")
     }
-        return userNum;
+        console.log(userNum);
+        return userNum;  
 }
+
+
 
 let num = takeInput();
 
+
 //make grid
-for(let i=0; i < (num*num); i++){
-    const grid = document.createElement('div')
-    grid.classList.add("grid");
-    container.appendChild(grid);
-    let percent = 600/num;
-    grid.style.width = percent-2 + "px";
-    grid.style.height = percent-2 + "px";
+function getGrid(){
+    let width = document.getElementById("container").clientWidth - 40; // 40 = padding in CSS
+    for(let i=0; i < (num*num); i++){
+        const grid = document.createElement('div')
+        grid.classList.add("grid");
+        container.appendChild(grid);
+        let percent = (width/num);
+        grid.style.width = percent -1  + "px";
+        grid.style.height = percent -1 + "px";
+    }
 }
+
+
 
 const countAll = document.querySelectorAll('.grid').length;
 console.log(countAll);
 
+getGrid()
+
+// gridChoice.addEventListener('click', getGrid()); 
 
 //make hover action
 let box = container.children
@@ -34,5 +49,4 @@ for(let i=0; i < (num*num); i++){
     });
 }
 
-
-
+// gridChoice.addEventListener('click', takeInput); 
